828418025dbf16fcc8ef9303ee8fcb9ce9fcad0e
Fernando Reyes Ortiz <fernandoreyesortiz614@gmail.com>
2026-07-16 20:00:01 -0600

    Ejercicio CalificacionFinal resuelto

---
+++ ejerciciosXpLevel/calificacionFinal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(){
+
+    int calf1;
+    cin>> calf1;
+
+    int calf2;
+    cin >> calf2;
+
+    int calf3;
+    cin >> calf3;
+
+    float promedio = (calf1 + calf2 + calf3)/3.0;
+
+    cout << promedio;
+    return 0;
+}
