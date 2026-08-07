2fe5d6c275048fbea16a84a68291c734765722bb
Fernando Reyes Ortiz <fernandoreyesortiz614@gmail.com>
2026-08-06 20:41:18 -0600

    Removí carpetas erróneas de rodrigo

--- OOP/Soluciones/Abstraction/Autos/Main.java
+++ OOP/Soluciones/Abstraction/Autos/Main.java
@@ -2,8 +2,19 @@ public class Main{
    public static void main(String args[]){
    
       Car bocho = new Car("bochido","volskwagen","HG-10","rojo",30000,false);
+      Car audi = new Car("audi","volskwagen","HG-10","rojo",30000,false);
+      Car tesla = new Car("tesla","volskwagen","HG-10","rojo",30000,true);
+
+      Car [] cars = new Car[3];
+      cars[0] = bocho;
+      cars[1] = audi;
+      cars[2] = tesla;
+
+
+      Car car =  new Car();
+      car.filteredSafeCars(cars);
       
-      System.out.println("El color del bocho es: " + bocho.color);
+      /*System.out.println("El color del bocho es: " + bocho.color);*/
    }
 }
 
@@ -17,6 +28,8 @@ class Car{
    double price;
    boolean isSafe;
    
+   //CONSTRUCTOR VACÍO
+   public Car(){}
    //CONSTRUCTOR
    public Car(String name, String brand, String model, String color, double price, boolean isSafe){
       this.name = name;
@@ -26,4 +39,14 @@ class Car{
       this.price = price;
       this.isSafe = isSafe;
    }
+
+   public void filteredSafeCars(Car [] cars){
+
+      for(Car car: cars){
+         if(car.isSafe){
+            System.out.println("Carro " + car.name + " listo para venderse");
+         }
+      }
+   }
+
 }
