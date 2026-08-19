b8013d7e314429349d584148498e46dba90be1e0
rubiook-hub <rubiogironcarlos@gmail.com>
2026-08-19 12:37:31 -0600

    agrego algoritmo ejercicio 1

---
+++ DST/Carlos/Ejercicio 1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+   public static void main(String [] args) {
+   
+   int suma = 0;
+   int tope = 4;
+   
+   for(int i = 1; i <= tope; i++) {
+      suma = suma + i;
+   }
+   
+   return suma;
+   
+   
+   
+   }
+}
